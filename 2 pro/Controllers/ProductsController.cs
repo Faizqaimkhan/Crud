4816abc6517439ad139a17844aed2cf9ad0348be
Faizqaimkhan <154994749+Faizqaimkhan@users.noreply.github.com>
2024-11-24 03:37:16 +0500

    Add project files.

---
+++ 2 pro/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using _2_pro.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2_pro.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.products.ToList();
+            return View(products);
+        }
+    }
+}
---
+++ 2 pro/Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _2_pro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2_pro.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+        public DbSet<Product> products { get; set; }
+    }
+}
